439fc947da85766b99b60985c65ea25e2bd2d47c
kiange <kiange@1ff43d9d-d119-0410-a323-7b9f0e15554d>
2007-06-14 08:07:19 +0000

    Added items remotely
    C:\Documents and Settings\kiang\桌面\5.0.3

---
+++ trunk/vtigercrm/5.0.3/modules/Administration/language/zh-tw.lang.php
@@ -0,0 +1,9 @@
+<?php
+$mod_strings = array (
+  'LBL_MODULE_NAME' => '管理',
+  'LBL_MODULE_TITLE' => '管理： 首頁',
+  'LBL_NEW_FORM_TITLE' => '新增帳號',
+  'ERR_DELETE_RECORD' => '在刪除帳號前必須輸入正確的記錄編號.',
+
+);
+?>
