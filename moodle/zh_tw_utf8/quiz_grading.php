f7c710e70778b2d4aa918355af0d971d1db051c5
kiange <kiange@1ff43d9d-d119-0410-a323-7b9f0e15554d>
2007-06-23 06:44:20 +0000

    Removed file/folder

--- moodle/zh_tw_utf8/quiz_grading.php
+++
@@ -1,8 +0,0 @@
-<?PHP // $Id: quiz_grading.php,v 1.1 2007/02/18 22:10:48 koenr Exp $ 
-      // quiz_grading.php - created with Moodle 1.6 + (2006050507)
-
-
-$string['essayonly'] = '下面題目必須手動評分';
-$string['grading'] = '手動評分';
-
-?>
