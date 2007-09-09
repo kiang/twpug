2c42fbeb048b9e2e0f109f385646269511600adc
kiange <kiange@1ff43d9d-d119-0410-a323-7b9f0e15554d>
2007-09-09 00:21:50 +0000

    Added SugarCE-Full-5.0.0 Beta1

---
+++ trunk/sugarcrm/SugarCE-Full-5.0.0/modules/DynamicFields/language/zh-tw.lang.php
@@ -0,0 +1,16 @@
+<?php
+if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
+$mod_strings = array (
+  'LNK_NEW_CALL' => '安排電話聯繫',
+  'LNK_NEW_MEETING' => '安排會議',
+  'LNK_NEW_TASK' => '建立任務',
+  'LNK_NEW_NOTE' => '建立記事或附加檔案',
+  'LNK_NEW_EMAIL' => '建立郵件',
+  'LNK_CALL_LIST' => '電話',
+  'LNK_MEETING_LIST' => '會議',
+  'LNK_TASK_LIST' => '任務',
+  'LNK_NOTE_LIST' => '記事',
+  'LNK_EMAIL_LIST' => '郵件',
+
+);
+?>
---
+++ trunk/sugarcrm/SugarCE-Full-5.0.0/modules/ModuleBuilder/language/zh-tw.lang.php
@@ -0,0 +1,16 @@
+<?php
+if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
+$mod_strings = array (
+  'help' =>
+  array (
+    'package' => '',
+    'main' => '',
+    'module' => '',
+    'listViewEditor' => '',
+    'layoutEditor' => '',
+    'studioWizard' => '',
+  ),
+  'LBL_MODULEBUILDER' => '模組產生器',
+
+);
+?>
