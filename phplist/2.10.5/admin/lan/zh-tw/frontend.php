d224e0ce8bc4738743c620498fa9f17040839fd5
kiange <kiange@1ff43d9d-d119-0410-a323-7b9f0e15554d>
2007-09-10 03:58:20 +0000

    1. Update phplist/texts/tchinese.inc & translate.php 2. Add phplist 2.10.5

---
+++ phplist/2.10.5/admin/lan/zh-tw/frontend.php
@@ -0,0 +1,8 @@
+<?php
+$lan = array (
+  'Please indicate how often you want to receive messages' => '請定義您希望收到訊息的頻率',
+  'Email is blacklisted, so request for confirmation has been sent.' => '帳號在黑名單中，確認訊息已經寄出',
+  'If user confirms subscription, they will be removed from the blacklist.' => '如果使用者透過信件確認，他們將會自黑名單中移除',
+
+);
+?>
