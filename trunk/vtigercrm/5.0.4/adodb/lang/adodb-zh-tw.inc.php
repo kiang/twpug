6bccd0a18129b8016c6e658e448c6118780f9e6b
kiange <kiange@1ff43d9d-d119-0410-a323-7b9f0e15554d>
2007-10-13 00:52:46 +0000

    Updated for 5.0.4

---
+++ trunk/vtigercrm/5.0.4/adodb/lang/adodb-zh-tw.inc.php
@@ -0,0 +1,34 @@
+<?php
+
+$ADODB_LANG_ARRAY = array (
+			'LANG'                      => 'zh-tw',
+            DB_ERROR                    => '不知名的錯誤',
+            DB_ERROR_ALREADY_EXISTS     => '已經存在',
+            DB_ERROR_CANNOT_CREATE      => '無法建立',
+            DB_ERROR_CANNOT_DELETE      => '無法刪除',
+            DB_ERROR_CANNOT_DROP        => '無法卸載',
+            DB_ERROR_CONSTRAINT         => '違反規則',
+            DB_ERROR_DIVZERO            => '除數為零',
+            DB_ERROR_INVALID            => '錯誤',
+            DB_ERROR_INVALID_DATE       => '錯誤的日期或時間',
+            DB_ERROR_INVALID_NUMBER     => '錯誤的數字',
+            DB_ERROR_MISMATCH           => '不符合',
+            DB_ERROR_NODBSELECTED       => '沒有選擇資料庫',
+            DB_ERROR_NOSUCHFIELD        => '沒有這個欄位',
+            DB_ERROR_NOSUCHTABLE        => '沒有這個資料表',
+            DB_ERROR_NOT_CAPABLE        => '資料庫後端無法運作',
+            DB_ERROR_NOT_FOUND          => '找不到',
+            DB_ERROR_NOT_LOCKED         => '未鎖定',
+            DB_ERROR_SYNTAX             => '語法錯誤',
+            DB_ERROR_UNSUPPORTED        => '不支援',
+            DB_ERROR_VALUE_COUNT_ON_ROW => '資料列數量',
+            DB_ERROR_INVALID_DSN        => '錯誤的 DSN',
+            DB_ERROR_CONNECT_FAILED     => '連線錯誤',
+            0	                       => '沒有錯誤', // DB_OK
+            DB_ERROR_NEED_MORE_DATA     => '提供的資料不足',
+            DB_ERROR_EXTENSION_NOT_FOUND=> '外掛找不到',
+            DB_ERROR_NOSUCHDB           => '沒有資料庫',
+            DB_ERROR_ACCESS_VIOLATION   => '權限不足'
+);
+?>
+		
---
+++ trunk/vtigercrm/5.0.4/include/fckeditor/editor/plugins/placeholder/lang/zh-tw.js
@@ -0,0 +1,21 @@
+﻿/*
+ * FCKeditor - The text editor for internet
+ * Copyright (C) 2003-2005 Frederico Caldeira Knabben
+ * 
+ * Licensed under the terms of the GNU Lesser General Public License:
+ * 		http://www.opensource.org/licenses/lgpl-license.php
+ * 
+ * For further information visit:
+ * 		http://www.fckeditor.net/
+ * 
+ * File Name: zh-tw.js
+ * 	Placholder English language file.
+ * 
+ * File Authors:
+ * 		Finjon Kiang [http://twpug.net/]
+ */
+FCKLang.PlaceholderBtn			= '插入/編輯置換標籤' ;
+FCKLang.PlaceholderDlgTitle		= '置換標籤屬性' ;
+FCKLang.PlaceholderDlgName		= '置換標籤名稱' ;
+FCKLang.PlaceholderErrNoName	= '請輸入置換標籤名稱' ;
+FCKLang.PlaceholderErrNameInUse	= '指定的名稱已經存在' ;
