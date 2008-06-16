5208104d3d66fd0f1eaf7500003712abf1f64807
kiange <kiange@1ff43d9d-d119-0410-a323-7b9f0e15554d>
2008-06-16 13:54:18 +0000

    Update to 5.1 RC

---
+++ trunk/sugarcrm/SugarCE-Full-5.1.0/translate/dumppack/manifest.php
@@ -0,0 +1,24 @@
+<?php 
+$manifest = array( 
+	'name' => 'Traditional Chinese',
+	'description' => '20080423',
+	'type' => 'langpack',
+	'is_uninstallable' => 'Yes',
+	'version' => '20080423',
+	'published_date' => '20080423',
+	'author' => 'Finjon Kiang',
+);
+
+$installdefs = array(
+	'id'=> 'zh-tw',
+        
+	'image_dir'=>'<basepath>/images',
+        
+	'copy' => array(
+        
+	array('from'=> '<basepath>/include','to'=> 'include',),
+        
+	array('from'=> '<basepath>/modules','to'=> 'modules'))
+        
+);
+?>
