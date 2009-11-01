9c83803adfca300f7fab501161d18bcb0167cbc6
kiange <kiange@1ff43d9d-d119-0410-a323-7b9f0e15554d>
2009-11-01 08:44:04 +0000

    Add translation of Elgg

---
+++ trunk/elgg/mod/embed/languages/zh.php
@@ -0,0 +1,18 @@
+<?php
+
+	$chinese = array(
+
+		'media:insert' => '嵌入/上傳多媒體檔案',
+
+		'embed:instructions' => 'Click on any file to embed it into your content.',
+
+		'embed:media' => '嵌入多媒體檔案',
+		'upload:media' => '上傳多媒體檔案',
+
+		'embed:file:required' => 'No file upload facilities were found. The system administrator may need to upload the file plugin or similar.',
+
+	);
+
+	add_translation("zh",$chinese);
+
+?>
