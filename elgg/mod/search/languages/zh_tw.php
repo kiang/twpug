e309c704f2bc57fe7d091680517956d53b8b4cf5
kiange <kiange@1ff43d9d-d119-0410-a323-7b9f0e15554d>
2010-04-25 03:11:58 +0000

    Commit elgg translation with the version provided by GGYY( 1.7 )

---
+++ elgg/mod/search/languages/zh_tw.php
@@ -0,0 +1,23 @@
+<?php
+
+$traditionalChinese = array(
+
+	'search:enter_term' => '請輸入要搜尋的字彙:',
+	'search:no_results' => '未發現任何相符的結果.',
+	'search:matched' => '符合的項目: ',
+	'search:results' => '%s的結果',
+	'search:no_query' => '請輸入要查詢的內容.',
+	'search:search_error' => '錯誤',
+
+	'search:more' => '+%s 更多 %s',
+
+	'search_types:tags' => '標籤',
+
+	'search_types:comments' => '評論',
+	'search:comment_on' => '"%s"的評論',
+	'search:unavailable_entity' => '無法使用的項目',
+	);
+
+add_translation("zh_tw",$traditionalChinese);
+
+?>
