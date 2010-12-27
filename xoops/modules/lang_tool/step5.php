62b123832363fc3ebc3a74acb0b3ec63a073939d
kiange <kiange@1ff43d9d-d119-0410-a323-7b9f0e15554d>
2010-12-27 05:28:56 +0000

    Use new function to do language file parsing

--- xoops/modules/lang_tool/step5.php
+++ xoops/modules/lang_tool/step5.php
@@ -27,8 +27,22 @@ $translated_str = '';
 switch ($_POST['ext']) {
     case 'php':
         $file_from = $_SESSION['lang_tool']['base_dir'] . $_SESSION['lang_tool']['from'] . $_SESSION['lang_tool']['path'] . '/' . $_SESSION['lang_tool']['file'];
-        $source_str = file_get_contents($file_from);
-        $translated_str = preg_replace_callback(LANG_TOOL_PATTERN, 'lang_trans', $source_str);
+        $match1 = parseLangFile($file_from);
+        $fileFromContent = file_get_contents($file_from);
+        $currentPosition = 0;
+        foreach($match1 AS $key => $val) {
+            $betweenLength = $val['start'] - $currentPosition;
+            if($betweenLength > 0) {
+                $translated_str .= substr($fileFromContent, $currentPosition, $betweenLength);
+                $currentPosition = $val['start'];
+            }
+            if(!empty($_POST['lang'][$key])) {
+                $val['content'] = $_POST['lang'][$key];
+            }
+            $translated_str .= sprintf('define(\'%s\', %s);', $key, $val['content']);
+            $currentPosition = $val['end'];
+        }
+        $translated_str .= substr($fileFromContent, $currentPosition);
         break;
     case 'tpl':
         $translated_str = $_POST['target'];
