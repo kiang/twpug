fc44bc47b5ba958d19e7331ff6db094962fd6c3b
kiange <kiange@1ff43d9d-d119-0410-a323-7b9f0e15554d>
2011-02-10 13:57:28 +0000

    Add translation for vanilla forum

---
+++ trunk/vanilla/zh_TW/definitions.php
@@ -0,0 +1,12 @@
+<?php if (!defined('APPLICATION')) exit();
+
+$LocaleInfo['zh_TW'] = array(
+   'Locale' => 'zh-TW',
+   'Name' => 'Traditional Chinese (正體中文)',
+   'Description' => 'Traditional Chinese translation for using in Taiwan',
+   'Version' => '1.2.1',
+   'Author' => 'TWPUG',
+   'AuthorEmail' => 'kiang@osobiz.com',
+   'AuthorUrl' => 'http://twpug.net',
+   'License' => 'GPLv3'
+);
